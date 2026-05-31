4a73ec2e613f615d64f805f0ae761b7f933e0116
owaisahmed123 <owais010.com@gmail.com>
2026-05-31 21:33:18 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-
   const [input, setInput] = useState("");
   const [todo, setTodo] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    if (input.trim() === "") return;
+
+    if (editIndex !== null) {
+      const copyTodo = [...todo];
+      copyTodo[editIndex] = input;
+      setTodo(copyTodo);
+      setEditIndex(null);
+    } else {
+      setTodo([...todo, input]);
+    }
+
+    setInput("");
+  };
 
   const deleteTodo = (indexValue) => {
     const updatedTodo = todo.filter((item, index) => {
@@ -14,40 +31,56 @@ function App() {
     setTodo(updatedTodo);
   };
 
+  const editTodo = (indexValue) => {
+    setInput(todo[indexValue]);
+    setEditIndex(indexValue);
+  };
+
   return (
     <div className="App">
-
-      <form onSubmit={(e) => {
-        e.preventDefault();
-
-        // if (input.trim() === "") return;
-
-        setTodo([...todo, input]);
-        setInput("");
-      }}>
-
+      <form onSubmit={addTodo}>
         <input
           type="text"
+          placeholder="Enter Todo..."
           value={input}
-          placeholder="Enter todo..."
           onChange={(e) => setInput(e.target.value)}
         />
 
-        <button type="submit">Add</button>
+        <button>
+          {editIndex !== null ? "Update" : "Add"}
+        </button>
       </form>
 
-      {todo.map((eachInput, index) => {
-        return (
-          <div className="todo-item" key={index}>
-            <h4>{eachInput}</h4>
+      {todo.length === 0 ? (
+        <h4 className="empty">No Todos Added Yet</h4>
+      ) : (
+        todo.map((item, index) => {
+          console.log(item)
+          return (
+            <div className="todo-item" key={index}>
+              <h4>{item}</h4>
 
-            <button onClick={() => deleteTodo(index)}>
-              Delete
-            </button>
-          </div>
-        );
-      })}
+              <div className="todo-actions">
+                <button
+                  className="edit-btn"
+                  onClick={() => editTodo(index)}
+                  type="button"
+                >
+                  Edit
+                </button>
 
+                <button
+                  className="delete-btn"
+                  onClick={() => deleteTodo(index)}
+                  type="button"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
